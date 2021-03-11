245df9ab5fafd3c2060571c491a1518cd3152e8b
Simurgh <62325871+simurgh314@users.noreply.github.com>
2021-03-11 21:54:25 +0500

    Update razmetka.sh

--- razmetka.sh
+++ razmetka.sh
@@ -6,15 +6,19 @@
 
 (
 	
-unit mib;
-mklabel gpt;
-mkpart primary 2 258;		#1 /dev/vda1 256MB EFI (FAT32)
-set 1 boot on;
-mkpart primary 258 514;		#2 /dev/vda2 256MB boot (ext2)
-name 2 boot;
-mkpart primary 514 4610;	#3 /dev/vda3 4GB (SWAP)
-name 3 swap
-mkpart primary 4610 -1;		#4 /dev/vda4 (ext4)
-name 4 root;
+echo 'unit mib';
+echo 'mklabel gpt';
+echo 'mkpart primary 2 258';		#1 /dev/vda1 256MB EFI (FAT32)
+echo 'set 1 boot on';
+echo 'mkpart primary 258 514';		#2 /dev/vda2 256MB boot (ext2)
+echo 'name 2 boot';
+echo 'mkpart primary 514 4610';		#3 /dev/vda3 4GB (SWAP)
+echo 'name 3 swap';
+echo 'mkpart primary 4610 -1';		#4 /dev/vda4 (ext4)
+echo 'name 4 root';
 
 ) | parted -a optimal /dev/vda  # выравнивание разделов
+
+print
+quit
+
