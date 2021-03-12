562b8d89590c21ca18ec01509982dedd4c4d8ad0
Simurgh <62325871+simurgh314@users.noreply.github.com>
2021-03-12 18:42:09 +0500

    Update start1.sh

--- start1.sh
+++ start1.sh
@@ -14,6 +14,11 @@ echo '
 ###################################################################
 '
 
+#не забыть поменять на sda
+
+export DISK="vda" &&
+export LANG="ru_RU.UTF-8" &&
+export MODULE="" &&
 
 loadkeys ru &&
 setfont cyr-sun16 &&
@@ -23,78 +28,92 @@ echo 'ru_RU.KOI8-R KOI8-R' >> /etc/locale.gen &&
 echo 'ru_RU.UTF-8 UTF-8' >> /etc/locale.gen &&
 echo 'ru_RU ISO-8859-5' >> /etc/locale.gen &&
 locale-gen &&
-export LANG=ru_RU.UTF-8 &&
 timedatectl set-ntp true &&
-pacman -Sy wget parted &&
-
 
 
 echo  'РАЗМЕТКА'
 
-# требуются wget и parted
-# не забыть изменить vda на sda
-
 (
-	
-echo 'unit mib';
-echo 'mklabel gpt';
-echo 'mkpart primary 2 258';		#1 /dev/vda1 256MB EFI (FAT32)
-echo 'set 1 boot on';
-echo 'mkpart primary 258 514';		#2 /dev/vda2 256MB boot (ext2)
-echo 'name 2 boot';
-echo 'mkpart primary 514 4610';		#3 /dev/vda3 4GB (SWAP)
-echo 'name 3 swap';
-echo 'mkpart primary 4610 -1';		#4 /dev/vda4 (ext4)
-echo 'name 4 root';
-print
 
-) | parted -a optimal /dev/vda  # выравнивание разделов
+echo g;
+echo n;
+echo ;
+echo ;
+echo +256M; #EFI
+echo Y;
+echo t;
+echo 1;
 
+
+echo n;
+echo ;
+echo ;
+echo +256M; #boot
+echo Y;
+ 
+ 
+echo n;
+echo ;
+echo ;
+echo +4G; #swap
+echo Y;
+  
+  
+echo n;
+echo ;
+echo ;
+echo ; #root
+echo Y;
+echo w;
+
+) | fdisk -t gpt /dev/$DISK
 
 
 echo 'ФОРМАТИРОВАНИЕ'
 
-mkfs.fat -F32 /dev/vda1 &&			 #EFI  (FAT32)
-mkfs.ext2 /dev/vda2	&&			 #boot (ext2)
-mkfs.ext4 /dev/vda4	&&			 #root (ext4)
-mkswap /dev/vda3 &&				 #swap
-swapon /dev/vda3 &&
-
+mkswap /dev/${DISK}3 &&
+swapon /dev/${DISK}3 &&
+mkfs.fat -F32 /dev/${DISK}1 &&
+(echo y;) | mkfs.ext2 /dev/${DISK}2	&&
+(echo y;) | mkfs.ext4 /dev/${DISK}4	&&
 
 
 echo 'МОНТИРОВАНИЕ'
 
-mount /dev/vda4 /mnt &&
+mount /dev/${DISK}4 /mnt &&
 mkdir -p /mnt/{home,boot} &&
-mount /dev/vda2 /mnt/boot &&
+mount /dev/${DISK}2 /mnt/boot &&
 lsblk
-
 
 
 echo 'УСТАНОВКА БАЗОВОЙ СИСТЕМЫ'
 
+clear &&
 pacstrap /mnt linux-firmware linux-zen-headers linux-zen base base-devel nano dhcpcd dialog wpa_supplicant netctl mc git wget &&   
 genfstab -U /mnt >> /mnt/etc/fstab
 
 
-
 echo 'ПЕРЕХОД В НОВОЕ ОКРУЖЕНИЕ'
 
-arch-chroot  /mnt  /bin/bash 
-echo 'HOOKS=(base udev autodetect modconf block keymap filesystems keyboard fsck)' >> /etc/mkinitcpio.conf	
-
+arch-chroot /mnt /bin/bash 
+echo "MODULES=($MODULE)" > /etc/mkinitcpio.conf
+echo "BINARIES=()" >> /etc/mkinitcpio.conf
+echo "FILES=()" >> /etc/mkinitcpio.conf
+echo "HOOKS=(base udev autodetect modconf block keymap filesystems keyboard fsck)" >> /etc/mkinitcpio.conf
 
 
 echo 'СОЗДАНИЕ ЯДРА И ЗАГРУЗЧИКА'
 
-cd /boot && mkinitcpio -p linux &&
+cd /boot && mkinitcpio -p linux-zen &&
 pacman -Syu grub efibootmgr dosfstools os-prober mtools &&
-mkdir /boot/EFI
-mount /dev/vda1  /boot/EFI
+mkdir /boot/EFI &&
+mount /dev/${DISK}1 /boot/EFI &&
 grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck &&
 grub-mkconfig -o /boot/grub/grub.cfg &&
 
-echo 'ЗАДАЙТЕ ПАРОЛЬ АДМЭНА :'
+clear &&
+echo '                       '                                                              
+echo 'ЗАДАЙТЕ ПАРОЛЬ АДМИНА :'
 passwd
 
 #exit
