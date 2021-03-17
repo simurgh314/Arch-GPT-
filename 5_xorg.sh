3702712f5ab46cd6df8d760f8e862c4694826a03
Simurgh <62325871+simurgh314@users.noreply.github.com>
2021-03-17 21:25:47 +0500

    Update 5_xorg.sh

--- 5_xorg.sh
+++ 5_xorg.sh
@@ -3,4 +3,4 @@
 sudo pacman -Syu --noconfirm xorg-server xorg-xinit xorg-apps xterm mesa-libgl &&
 sudo pacman -S --noconfirm ttf-font-awesome ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-uming ttf-hanazono ttf-arphic-ukai &&
 
-su $USER
+su $NEW_USER
