e5648c093ac36c924ec48e414eee45795f4b9b06
Simurgh <62325871+simurgh314@users.noreply.github.com>
2021-03-17 23:08:08 +0500

    Update 4_locale_user.sh

--- 4_locale_user.sh
+++ 4_locale_user.sh
@@ -37,7 +37,4 @@ su $NEW_USER
 
 echo 'УСТАНОВКА Xorg'
 pacman -Syu --noconfirm xorg-server xorg-xinit xorg-apps xterm mesa-libgl &&
-pacman -S --noconfirm ttf-font-awesome ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-uming ttf-hanazono ttf-arphic-ukai
-
-
 
