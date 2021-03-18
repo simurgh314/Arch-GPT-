9e72fe8e565e04ae8f1baf1a07a53b64e74a1e09
Simurgh <62325871+simurgh314@users.noreply.github.com>
2021-03-18 15:43:10 +0500

    Update 5_pakety.sh

--- 5_pakety.sh
+++ 5_pakety.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 
 echo 'УСТАНОВКА ПРОГРАММ'
-sudo pacman -Syu --noconfirm xorg-server xorg-xinit xorg-apps xterm mesa-libgl ttf-font-awesome ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-uming ttf-hanazono ttf-arphic-ukai i3-gaps rofi i3status nitrogen flameshot xfce4-clipman-plugin lxappearance tumbler numlockx firefox ristretto unzip kolourpaint usbutils ufw speedcrunch gpart gparted catfish neofetch htop xfce4-terminal gvfs alsa alsa-plugins alsa-utils pulseaudio pulseaudio-equalizer pulseaudio-jack pavucontrol pasystray
+sudo pacman -Syu --noconfirm 
+{xorg-server 
+xorg-xinit 
+xorg-apps 
+xterm 
+mesa-libgl 
+ttf-font-awesome 
+ttf-liberation 
+ttf-dejavu 
+opendesktop-fonts 
+ttf-bitstream-vera 
+ttf-arphic-uming 
+ttf-hanazono 
+ttf-arphic-ukai 
+i3-gaps 
+i3status 
+nitrogen
+xfce4-clipman-plugin 
+lxappearance 
+tumbler 
+numlockx 
+firefox 
+ristretto 
+unzip
+usbutils
+speedcrunch
+neofetch
+xfce4-terminal 
+gvfs
+}
 
-
+#rofi pasystray pavucontrol pulseaudio-jack pulseaudio-equalizer flameshot thunar
+#alsa-plugins alsa-utils pulseaudio alsa gpart gparted kolourpaint ufw htop catfish
