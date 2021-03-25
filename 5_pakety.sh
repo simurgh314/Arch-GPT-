0064adb15fead97b573a6eea9f05368c7c71da42
Simurgh <62325871+simurgh314@users.noreply.github.com>
2021-03-25 16:21:57 +0500

    Update 5_pakety.sh

--- 5_pakety.sh
+++ 5_pakety.sh
@@ -2,13 +2,52 @@
 
 https://git.io/JYIk9
 
-cd &&
 
-echo 'УСТАНОВКА ПРОГРАММ'
+cd && echo 'УСТАНОВКА ПРОГРАММ'
 
-sudo pacman -Syu --noconfirm xorg-server xorg-xinit xorg-apps xterm mesa-libgl alsa-plugins alsa-utils pulseaudio alsa gpart gparted kolourpaint ufw htop catfish firefox ttf-font-awesome ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-uming ttf-hanazono ttf-arphic-ukai i3-gaps i3status nitrogen xfce4-clipman-plugin lxappearance tumbler numlockx ristretto unzip usbutils speedcrunch neofetch xfce4-terminal gvfs rofi pasystray pavucontrol pulseaudio-jack pulseaudio-equalizer flameshot thunar 
+packets='
+xorg-server 
+xorg-xinit 
+xorg-apps 
+xterm mesa-libgl 
+ttf-font-awesome 
+ttf-liberation 
+ttf-dejavu 
+opendesktop-fonts
+ttf-bitstream-vera 
+ttf-arphic-uming 
+ttf-hanazono 
+ttf-arphic-ukai 
+i3-gaps 
+nitrogen 
+xfce4-clipman-plugin
+lxappearance 
+tumbler 
+numlockx 
+firefox 
+ristretto 
+unzip 
+usbutils 
+speedcrunch 
+neofetch 
+xfce4-terminal 
+gvfs
+rofi 
+pasystray 
+pavucontrol 
+pulseaudio-jack 
+pulseaudio-equalizer 
+flameshot 
+thunar
+alsa-plugins 
+alsa-utils 
+pulseaudio 
+alsa 
+gpart 
+gparted 
+kolourpaint 
+ufw 
+htop 
+catfish'
 
-
-
-#
-#
+sudo pacman -Syu --noconfirm $packets
